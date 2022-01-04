a9f6de7ff7e7844893e8092a20a1b04323be8127
wolray <wolray@foxmail.com>
2022-01-04 17:37:29 +0800

    Create DataStream.java

---
+++ src/main/java/com/github/wolray/line/io/DataStream.java
@@ -0,0 +1,140 @@
+package com.github.wolray.line.io;
+
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author ray
+ */
+public class DataStream<T> {
+    private final Class<T> type;
+    private Supplier<Stream<T>> supplier;
+    private List<T> ts;
+
+    DataStream(Class<T> type, Supplier<Stream<T>> supplier) {
+        this.type = type;
+        this.supplier = supplier;
+    }
+
+    private DataStream<T> mod(UnaryOperator<Stream<T>> op) {
+        if (ts != null) {
+            throw new IllegalStateException("cannot modify stream after caching");
+        }
+        Supplier<Stream<T>> old = supplier;
+        supplier = () -> op.apply(old.get());
+        return this;
+    }
+
+    public DataStream<T> limit(int maxSize) {
+        return mod(s -> s.limit(maxSize));
+    }
+
+    public DataStream<T> peek(Consumer<T> action) {
+        return mod(s -> s.peek(action));
+    }
+
+    public DataStream<T> filter(Predicate<T> predicate) {
+        return mod(s -> s.filter(predicate));
+    }
+
+    public DataStream<T> cache(String csvFile) {
+        return cache(csvFile, ",");
+    }
+
+    public DataStream<T> cache(String csvFile, String sep) {
+        LineCache<T> cache = new LineCache<>(sep, type);
+        ts = cache.get(csvFile, this::collect);
+        return this;
+    }
+
+    private List<T> collect() {
+        return supplier.get().collect(Collectors.toCollection(DataList::new));
+    }
+
+    public void forEach(Consumer<T> action) {
+        if (ts != null) {
+            ts.forEach(action);
+        } else {
+            supplier.get().forEach(action);
+        }
+    }
+
+    public List<T> toList() {
+        return ts != null ? ts : collect();
+    }
+
+    public List<T> toArrayList() {
+        return new ArrayList<>(toList());
+    }
+
+    public <K> Set<K> toSet(Function<T, K> mapper) {
+        List<T> ts = toList();
+        Set<K> set = new HashSet<>(ts.size());
+        ts.forEach(t -> set.add(mapper.apply(t)));
+        return set;
+    }
+
+    public <K, V> Map<K, V> toMap(Function<T, K> keyMapper, Function<T, V> valueMapper) {
+        List<T> ts = toList();
+        Map<K, V> map = new HashMap<>(ts.size());
+        ts.forEach(t -> map.put(keyMapper.apply(t), valueMapper.apply(t)));
+        return map;
+    }
+
+    public <K, V> Map<K, V> groupBy(Function<T, K> keyMapper, Collector<T, ?, V> collector) {
+        Stream<T> stream = ts != null ? ts.stream() : supplier.get();
+        return stream.collect(Collectors.groupingBy(keyMapper, collector));
+    }
+
+    public static class DataList<T> extends AbstractList<T> {
+        private final Node<T> dummy = new Node<>();
+        private Node<T> last = dummy;
+        private int size = 0;
+
+        @Override
+        public boolean add(T t) {
+            Node<T> node = new Node<>();
+            node.t = t;
+            last.next = node;
+            last = node;
+            size++;
+            return true;
+        }
+
+        @Override
+        public T get(int index) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public int size() {
+            return size;
+        }
+
+        @Override
+        public Iterator<T> iterator() {
+            return new Iterator<T>() {
+                Node<T> node = dummy;
+
+                @Override
+                public boolean hasNext() {
+                    return node.next != null;
+                }
+
+                @Override
+                public T next() {
+                    node = node.next;
+                    return node.t;
+                }
+            };
+        }
+    }
+
+    private static class Node<T> {
+        T t;
+        Node<T> next;
+    }
+}
