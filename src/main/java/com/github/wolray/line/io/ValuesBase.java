e8dae277164a82d70eac3fba187bc0dd06cbf2c9
yilai <yilai.wl@cainiao.com>
2022-01-05 22:57:27 +0800

    [fix] del LineCache [opt] FieldContext

--- src/main/java/com/github/wolray/line/io/ValuesBase.java
+++ src/main/java/com/github/wolray/line/io/ValuesBase.java
@@ -1,10 +1,9 @@
 package com.github.wolray.line.io;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Member;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
+import java.lang.reflect.*;
 import java.util.*;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
 import java.util.stream.Stream;
 
 /**
@@ -78,5 +77,51 @@ class ValuesBase<T> {
             }
         }
         return stream;
+    }
+
+    public static class FieldContext {
+        public final Field field;
+        UnaryOperator<String> mapper;
+        Function<String, ?> parser;
+        Function<Object, ?> function;
+        Function<Object, String> formatter;
+
+        FieldContext(Field field) {
+            this.field = field;
+        }
+
+        void composeMapper() {
+            if (mapper != null) {
+                parser = parser.compose(mapper);
+            }
+        }
+
+        public Object convertString(String s) {
+            return parser.apply(s);
+        }
+
+        public Object convertObject(Object o) {
+            return function.apply(o);
+        }
+
+        void set(Object t, Object o) {
+            try {
+                field.set(t, o);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        Object get(Object t) {
+            try {
+                return field.get(t);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        String format(Object t) {
+            return formatter.apply(get(t));
+        }
     }
 }
