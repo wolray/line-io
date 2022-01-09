ec145bb53a2fe135126220f905a366044101a05f
ray <wolray@foxmail.com>
2022-01-09 23:34:38 +0800

    [add] readme

---
+++ src/test/java/com/github/wolray/line/io/demo/DemoChnTest.java
@@ -0,0 +1,94 @@
+package com.github.wolray.line.io.demo;
+
+import com.github.wolray.line.io.DataStream;
+import com.github.wolray.line.io.Fields;
+import com.github.wolray.line.io.LineReader;
+import com.github.wolray.line.io.LineWriter;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @author wolray
+ */
+public class DemoChnTest {
+    @Test
+    public void demo() {
+        // 从本地文件快速获取inputStream（如果文件不存在在返回null
+        InputStream inputStream = LineReader.toInputStream("some-path/person.csv");
+        // 设置CSV的分隔符，以及目标class
+        List<Person> persons = LineReader.byCsv(",", Person.class)
+            // 如果不需要第一行的列名信息，可以直接跳过
+            .read(inputStream, 1)
+            // 如果文件的列名和class的属性顺序不一致，可以手动设置列名，读取时会自动匹配
+            .csvHeader("name", "gender", "age", "weight", "height", "phone")
+            // 也可以通过下标的方式设置每个属性对应哪一列
+            .columns(0, 1, 2, 4, 6, 8)
+            // 也可以通过excel的列名方式来声明
+            .columns("A,B,C,E,G,I")
+            // 生成一个惰性加载的stream，只有在collect才会真正执行
+            .stream()
+            // 可以在初始化之后随意计算其他属性
+            .peek(p -> p.weightHeightRatio = p.weight / p.height)
+            // 过滤年龄大于18的对象
+            .filter(p -> p.age >= 18)
+            // 把这些对象缓存到另一个csv文件里
+            // 下次执行这段代码的时候，如果缓存文件存在则直接从文件里读取
+            .cacheCsv(",", Person.class, "some-path/person_18.csv")
+            // 转成一个基于单向链表的list，随着数量增加对内存更友好
+            .toList();
+
+        // 可以将list直接转成DataStream
+        DataStream<Person> stream = DataStream.of(persons)
+            // 像普通stream那样限制10个
+            .limit(10)
+            // 让该stream可以重复使用
+            .reuse();
+
+        // 把stream映射为一个名称列表并转为ArrayList
+        List<String> names = stream.map(p -> p.name).toArrayList();
+        // 生成一个以name为key的set
+        Set<String> personSet = stream.toSet(p -> p.name);
+        // 生成一个以name为key的map
+        Map<String, Person> personMap = stream.toMap(p -> p.name, p -> p);
+        // 按照gender分组
+        Map<Integer, List<Person>> genderGroupMap = stream.groupBy(p -> p.gender, Collectors.toList());
+
+        // 把persons对象另存到一个csv文件里
+        LineWriter.byCsv(",", Person.class).writeAsync(persons, "some-path/person_dump.csv");
+    }
+
+    /**
+     * 设置一个临时的DO类，声明public属性以便于使用（推荐方式
+     * 将其他的需要二次计算的属性声明为private，就能在转化时自动跳过
+     */
+    public static class Person {
+        public String name;
+        public Integer gender;
+        public Integer age;
+        public Double weight;
+        public Double height;
+        private Double weightHeightRatio;
+    }
+
+    /**
+     * 对于更常用的pojo类，打上一个@Fields(pojo = true)的注解即可
+     * pojo类同上述推荐的方式恰恰相反，只识别private的字段
+     */
+    @Fields(pojo = true)
+    public static class PojoPerson {
+        private String name;
+        private Integer gender;
+        private Integer age;
+        private Double weight;
+        private Double height;
+        /**
+         * 设置transient修饰符用于忽略属性
+         */
+        private transient Double weightHeightRatio;
+    }
+}
---
+++ src/test/java/com/github/wolray/line/io/demo/DemoTest.java
@@ -0,0 +1,97 @@
+package com.github.wolray.line.io.demo;
+
+import com.github.wolray.line.io.DataStream;
+import com.github.wolray.line.io.Fields;
+import com.github.wolray.line.io.LineReader;
+import com.github.wolray.line.io.LineWriter;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @author wolray
+ */
+public class DemoTest {
+    @Test
+    public void demo() {
+        // a quick util to get inputStream from a local file (null if the file doesn't exists
+        InputStream inputStream = LineReader.toInputStream("some-path/person.csv");
+        // set csv separator and target object class
+        List<Person> persons = LineReader.byCsv(",", Person.class)
+            // skip 1 line if you don't need the header
+            .read(inputStream, 1)
+            // if you need specify the header (file columns doesn't align with the class fields)
+            .csvHeader("name", "gender", "age", "weight", "height", "phone")
+            // or you may like to specify the columns by indices
+            .columns(0, 1, 2, 4, 6, 8)
+            // or using excel-style columns
+            .columns("A,B,C,E,G,I")
+            // generate a lazy stream that will not be evaluated until collecting.
+            .stream()
+            // computing properties right after converting the object
+            .peek(p -> p.weightHeightRatio = p.weight / p.height)
+            // filter 18+ aged persons
+            .filter(p -> p.age >= 18)
+            // cache these persons into another csv file
+            // next time when running the code the stream will be collected from the cached file
+            .cacheCsv(",", Person.class, "some-path/person_18.csv")
+            // collect a singly-linked list rather than an ArrayList or LinkedList for better memory efficiency
+            .toList();
+
+        // get a stream directly from a list
+        DataStream<Person> stream = DataStream.of(persons)
+            // limit 10 instances
+            .limit(10)
+            // make this stream reusable
+            .reuse();
+
+        // map to a name stream and collect to an array list
+        List<String> names = stream.map(p -> p.name).toArrayList();
+        System.out.println(names);
+        // generate a person set with name as the key
+        Set<String> personSet = stream.toSet(p -> p.name);
+        System.out.println(personSet);
+        // generate a person map with name as the key
+        Map<String, Person> personMap = stream.toMap(p -> p.name, p -> p);
+        System.out.println(personMap);
+        // groupby persons by their gender
+        Map<Integer, List<Person>> genderGroupMap = stream.groupBy(p -> p.gender, Collectors.toList());
+        System.out.println(genderGroupMap);
+
+        // write persons to another csv file
+        LineWriter.byCsv(",", Person.class).writeAsync(persons, "some-path/person_dump.csv");
+    }
+
+    /**
+     * define a temp DO class with public fields, which is easy to access than pojo (recommended way)
+     * define extra private fields that is omitted during the conversion and could be computed afterwards
+     */
+    public static class Person {
+        public String name;
+        public Integer gender;
+        public Integer age;
+        public Double weight;
+        public Double height;
+        private Double weightHeightRatio;
+    }
+
+    /**
+     * adding a @Fields annotation and set pojo = true if it is a pojo (only private fields are considered)
+     */
+    @Fields(pojo = true)
+    public static class PojoPerson {
+        private String name;
+        private Integer gender;
+        private Integer age;
+        private Double weight;
+        private Double height;
+        /**
+         * set transient to omit the field
+         */
+        private transient Double weightHeightRatio;
+    }
+}
