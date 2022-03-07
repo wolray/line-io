6ae16ba3b5acf36f3d7c0aeb0ed39c2283c46105
yilai <yilai.wl@cainiao.com>
2022-03-07 11:54:39 +0800

    [rename] replace

--- src/main/java/com/github/wolray/line/io/ext/Mutable.kt
+++ src/main/java/com/github/wolray/line/io/ext/Mutable.kt
@@ -19,10 +19,10 @@ fun <T> List<T>.asMutable() = this as MutableList
 fun <K, V> Map<K, V>.asMutable() = this as MutableMap
 fun <T> Set<T>.asMutable() = this as MutableSet
 
-inline fun <T> T.alterIf(condition: Boolean, block: T.() -> T): T {
+inline fun <T> T.replaceIf(condition: Boolean, block: T.() -> T): T {
     return if (condition) block() else this
 }
 
-inline fun <T, E> T.alterWith(e: E?, block: T.(E) -> T): T {
+inline fun <T, E> T.replaceWith(e: E?, block: T.(E) -> T): T {
     return if (e != null) block(e) else this
 }
