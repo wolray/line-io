4083b3e1fe6e54aa987812a7de948b067c491857
ray <wolray@foxmail.com>
2022-07-18 00:49:28 +0800

    [add] IReader.Csv

--- src/main/java/com/github/wolray/line/io/CsvReader.kt
+++ src/main/java/com/github/wolray/line/io/CsvReader.kt
@@ -1,20 +1,14 @@
 package com.github.wolray.line.io
 
-import com.github.wolray.line.io.TextScope.toSequence
-
 /**
  * @author wolray
  */
 class CsvReader<T> internal constructor(
     converter: ValuesConverter.Csv<T>,
     private val sep: String
-) : ValuesReader<Text, List<String>, T>(converter), IReader.Is<List<String>, T> {
+) : ValuesReader<Text, List<String>, T>(converter), IReader.Csv<T> {
 
-    override fun toIterator(source: Text): Iterator<List<String>> {
-        return source.toSequence().map(::split).iterator()
-    }
-
-    private fun split(s: String) = s.split(sep, limit = limit)
+    override fun split(s: String) = s.split(sep, limit = limit)
 
     override fun splitHeader(iterator: Iterator<List<String>>) = iterator.next()
 
--- src/main/java/com/github/wolray/line/io/IReader.kt
+++ src/main/java/com/github/wolray/line/io/IReader.kt
@@ -1,7 +1,9 @@
 package com.github.wolray.line.io
 
+import com.github.wolray.line.io.TextScope.toSequence
 import java.io.File
 import java.io.FileInputStream
+import java.net.URL
 
 /**
  * @author wolray
@@ -11,8 +13,17 @@ interface IReader<S, V, T> : (V) -> T {
     fun read(source: S): LineReader<S, V, T>.Session
 
     interface Is<V, T> : IReader<Text, V, T> {
+        fun read(url: URL) = read { url.openStream() }
         fun read(file: String) = read { FileInputStream(file) }
         fun read(file: File) = read { FileInputStream(file) }
         fun read(cls: Class<*>, resource: String) = read { cls.getResourceAsStream(resource)!! }
+    }
+
+    interface Csv<T> : Is<List<String>, T> {
+        fun split(s: String): List<String>
+
+        override fun toIterator(source: Text): Iterator<List<String>> {
+            return source.toSequence().map(::split).iterator()
+        }
     }
 }
